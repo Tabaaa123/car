105e1e8e604e92c22bd8cdcd13901b9120f78aeb
Ana <anaagr30@gmail.com>
2023-09-15 14:11:36 +0800

    first commit

---
+++ drive.js
@@ -0,0 +1,51 @@
+//open  modal
+function openModal(message) {
+    const modal = document.getElementById("myModal");
+    const modalText = document.getElementById("modalText");
+
+    modalText.textContent = message;
+    modal.style.display = "block";
+
+    // Close modal
+    const closeModal = document.getElementById("closeModal");
+    closeModal.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+
+    // Close modal when click outside
+    window.addEventListener("click", function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+}
+
+// Driver constructor function
+function Driver(name, age, workExperience) {
+    this.name = name;
+    this.age = age;
+    this.workExperience = workExperience;
+    this.driveMyCar = function () {
+        openModal(this.name  + " is driving the car" + " " + this.age + " years old " + " and " + this.workExperience + " Years Sports Racer" );
+    };
+    this.carWash = function () {
+        openModal(this.name + " is washing the car");
+    };
+}
+
+// Driver instances
+const Richard = new Driver("Richard", 25, 3);
+const Eli = new Driver("Eli", 30, 5);
+
+// Buttons
+const driveButton = document.getElementById("driveButton");
+const washButton = document.getElementById("washButton");
+
+// Event listeners
+driveButton.addEventListener("click", () => {
+    Richard.driveMyCar();
+});
+
+washButton.addEventListener("click", () => {
+    Eli.carWash();
+});
